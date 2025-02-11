5cccefe9796129a5ed3a1d916018e49ccfa5d36b
Aedan Gallivan <120228461+AedanG1@users.noreply.github.com>
2025-02-12 01:38:39 +1000

    changed bio

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -10,8 +10,9 @@ const Home = () => {
                     <h1 className="font-primary text-3xl text-neutral-500 dark:text-neutral-400">Aedan Gallivan</h1>
                     <h2 className="text-5xl font-semibold">Web Designer and  Developer</h2>
                     <h3 className="mt-4 text-xl text-neutral-500 dark:text-neutral-400">
-						With a foundation in design, I develop full-stack web applications
-						that prioritize user experience and achieve business goals.
+						With a foundation in design, I turn ideas into
+						engaging full-stack web applications by blending user-centered design
+						principles with modern web development.
                     </h3>
                 </div>
             </div>
