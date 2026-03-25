7e1360a79ea835729bbd66fca558d075b3ceb56c
Aedan Gallivan <120228461+AedanG1@users.noreply.github.com>
2026-03-25 20:02:19 +1000

    update resume

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -11,7 +11,7 @@ const Home = () => {
                     <h1 className="font-primary text-3xl text-neutral-500 dark:text-neutral-400">Aedan Gallivan</h1>
                     <h2 className="text-5xl font-semibold">Web Designer and Developer</h2>
                     <div className="mt-8">
-                        <a className="px-4 py-2 border border-neutral-400 inline-flex items-center gap-2" href="AedanGallivan-Resume2025.pdf" target="_blank" >
+                        <a className="px-4 py-2 border border-neutral-400 inline-flex items-center gap-2" href="Aedan_Gallivan_Resume.pdf" target="_blank" >
                             View Resumé <span>{Icons.ArrowTopRight("size-4")}</span>
                         </a>
                     </div>
