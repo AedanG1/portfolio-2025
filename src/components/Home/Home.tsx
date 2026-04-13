897b6a7c593ee17248bf709615a4476e399f3064
Aedan Gallivan <120228461+AedanG1@users.noreply.github.com>
2026-04-13 12:53:27 +1000

    update title

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -9,7 +9,7 @@ const Home = () => {
             <header className="flex md:flex-row">
                 <div className="md:w-1/2">
                     <h1 className="font-primary text-3xl text-neutral-500 dark:text-neutral-400">Aedan Gallivan</h1>
-                    <h2 className="text-5xl font-semibold">Web Designer and Developer</h2>
+                    <h2 className="text-5xl font-semibold">Full-Stack Developer<br></br>& Designer</h2>
                     <div className="mt-8">
                         <a className="px-4 py-2 border border-neutral-400 inline-flex items-center gap-2" href="Aedan_Gallivan_Resume.pdf" target="_blank" >
                             View Resumé <span>{Icons.ArrowTopRight("size-4")}</span>
