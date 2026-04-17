9082513d230be16dffb464caaddecbf6b6d6c534
Aedan Gallivan <120228461+AedanG1@users.noreply.github.com>
2026-04-17 20:16:59 +1000

    change contact page to column

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -2,12 +2,11 @@ import Icons from "../Icons/Icons";
 
 const Contact = () => {
     return (
-        <div className="flex flex-col mt-40 md:flex-row space-y-6 md:space-y-0">
+        <div className="flex flex-col mt-40 space-y-6">
             <div className="space-y-2 w-full md:w-1/2">
                 <h2 className="text-4xl font-semibold w-full md:w-4/6">Contact</h2>
             </div>
             <div className="space-y-2">
-                <h2 className="text-3xl text-nowrap w-full md:w-4/6 mb-4">Reach Out</h2>
                 <h3 className="text-xl">
                     <a 
                         className="flex flex-row items-center gap-2" 
