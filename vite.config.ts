9f439315b6381730f19564d3efd6097237354241
Aedan Gallivan <120228461+AedanG1@users.noreply.github.com>
2026-04-29 12:46:07 +1000

    add mdx and rehype plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import mdx from '@mdx-js/rollup'
 import { imagetools } from 'vite-imagetools'
+import rehypeSlug from 'rehype-slug'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+		mdx({ rehypePlugins: [ rehypeSlug ]}),
 	  react(),
 	  tailwindcss(),
 	  imagetools()
