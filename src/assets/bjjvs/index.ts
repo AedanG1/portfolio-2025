db89e335165dda394917fb0c399b6789078e591c
Aedan Gallivan <120228461+AedanG1@users.noreply.github.com>
2026-04-29 19:11:36 +1000

    move images into asset directories

---
+++ src/assets/bjjvs/index.ts
@@ -0,0 +1,7 @@
+export { default as aws } from './bjjvs-aws.png';
+export { default as editTags } from './bjjvs-edit-tags.png';
+export { default as editVideo } from './bjjvs-edit-video.png';
+export { default as filter } from './bjjvs-filter.png';
+export { default as hero } from './bjjvs-hero.png';
+export { default as thumbnail } from './bjjvs-hero.png';
+export { default as upload } from './bjjvs-upload.png';
