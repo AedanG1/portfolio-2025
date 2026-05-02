3d86838c65dc3b0d53a821b8ffa4c1db31c0fb36
Aedan Gallivan <120228461+AedanG1@users.noreply.github.com>
2026-05-02 22:09:27 +1000

    add ticker switch, add github activities component

--- src/components/GitHubActivityTicker.tsx
+++ src/components/GitHubActivityTicker.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
+import GitHubActivities from "./GitHubActivities";
 
-type GitHubEvent = {
+export type GitHubEvent = {
   id: string;
   type: string;
   repo: { name: string };
@@ -10,6 +11,16 @@ type GitHubEvent = {
 const GitHubActivityTicker = () => {
   const [events, setEvents] = useState<GitHubEvent[]>([]);
   const [error, setError] = useState<string | null>(null);
+  const [animationState, setAnimationState] = useState<boolean>(true);
+
+  let statusColor;
+  if (error) {
+    statusColor = "#FF0000";
+  } else if (!animationState) {
+    statusColor = "#FFB700";
+  } else {
+    statusColor = "#00FF1E";
+  }
 
   useEffect(() => {
     const controller = new AbortController();
@@ -17,7 +28,7 @@ const GitHubActivityTicker = () => {
     const apiCall = async () => {
       try {
         const response = await fetch(
-          "https://api.github.com/users/AedanG1/events/public",
+          "https://api.github.com/users/AedanG1/events/public?per_page=15",
           { signal: controller.signal }
         );
 
@@ -46,45 +57,53 @@ const GitHubActivityTicker = () => {
     return () => controller.abort();
   }, []);
 
-  const statusColor = error ? "#FF0000" : "#00FF1E";
+  const toggleAnimation = () => {
+    setAnimationState(!animationState);
+  };
 
   return (
     <>
     <div className="flex flex-row gap-4 items-center -mr-4">
-      <svg width={16} height={16} className="absolute blur-xs">
-        <circle cx={8} cy={8} r={4} fill={statusColor}/>
-      </svg>
-      <svg width={16} height={16} className="relative">
-        <circle cx={8} cy={8} r={4} fill={statusColor}/>
-          <path
-          d="M 4.5 6.9 A 4 5.2 0 0 1 11.4 6.9"
-          fill="none"
-          stroke="white"
-          strokeWidth={1}
-          strokeLinecap="round"
-          opacity={0.4}
-        />
-      </svg>
+      {/* Switch */}
+      <button 
+      onClick={() => toggleAnimation()}
+      className={`${animationState ? 'ticker-switch__on' : 'ticker-switch__off'} 
+      hover:cursor-pointer shadow-nav-button w-5 h-11 rounded-xs border border-black`}/>
+
+      {/* Status LED */}
+      <div>
+        <svg width={16} height={16} className="absolute blur-xs">
+          <circle cx={8} cy={8} r={4} fill={statusColor}/>
+        </svg>
+        <svg width={16} height={16} className="relative">
+          <circle cx={8} cy={8} r={4} fill={statusColor}/>
+            <path
+            d="M 4.5 6.9 A 4 5.2 0 0 1 11.4 6.9"
+            fill="none"
+            stroke="white"
+            strokeWidth={1}
+            strokeLinecap="round"
+            opacity={0.4}
+          />
+        </svg>
+      </div>
+
+      {/* Worn Label */}
       <span className="font-worn text-nav-button-subtle text-xs flex flex-col items-center">
         <span>GitHub</span>
         <span>Activity</span>
       </span>
+
     </div>
     <div className="font-terminal text-nav-button-louder bg-black py-2 rounded-lg
     border border-t-[#212121] border-l-[#212121] border-b-0 border-r-0
-    flex-1 min-w-0 overflow-hidden flex flex-row items-center group">
+    flex-1 min-w-0 overflow-hidden flex flex-row items-center">
       {error && <span>Couldn't load GitHub activity: {error}</span>}
       {!error && events.length === 0 && <span>Loading...</span>}
-      <div className="flex flex-row gap-12 w-max animated-ticker group-hover:[animation-play-state:paused]">
-        {[...events, ...events].map((event, i) => {
-          const createdAtAEST = new Date(event.created_at).toLocaleDateString("en-AU", { timeZone: "Australia/Brisbane" });
-          return (
-            // concatenate index to id to prevent duplicate keys
-            <div key={event.id + i} className="whitespace-nowrap">
-              {event.type}: {event.repo.name} - {createdAtAEST}
-            </div>
-          )
-        })}
+      <div 
+      style={{animationPlayState: animationState ? 'running' : 'paused'}}
+      className="flex flex-row gap-12 w-max animated-ticker">
+        <GitHubActivities events={events} />
       </div>
     </div>
     </>
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -5,18 +5,19 @@ import Icons from "../../assets/Icons/Icons";
 const Home = () => {
   return (
     <section className="space-y-24">
-      <header className="flex md:flex-row">
-        <div className="md:w-1/2">
+      <header className="flex flex-col">
           <h1 className="text-3xl text-body-normal">
             Aedan Gallivan
           </h1>
-          <span className="absolute pointer-events-none blur bg-linear-to-b from-moon/30 to-moon-light/30 bg-clip-text text-transparent text-5xl font-semibold py-2">
-            Full-Stack Developer<br></br>& Designer
-          </span>
-          <h2 className=" relative bg-linear-to-b from-moon to-moon-light bg-clip-text text-transparent text-5xl font-semibold py-2">
-            Full-Stack Developer<br></br>& Designer
-          </h2>
-          <p className="text-md mt-4">
+          <div>
+            <span className="absolute pointer-events-none blur bg-linear-to-b from-moon/30 to-moon-light/30 bg-clip-text text-transparent text-5xl font-semibold py-2">
+              Full-Stack Developer<br></br>& Designer
+            </span>
+            <h2 className="text-nowrap relative bg-linear-to-b from-moon to-moon-light bg-clip-text text-transparent text-5xl font-semibold py-2">
+              Full-Stack Developer<br></br>& Designer
+            </h2>
+          </div>
+          <p className="text-md mt-4 w-110">
             With a background in Product Design I take products from ideas to
             deployment, prioritizing user experience on the front end and clean,
             readable code throughout.
@@ -30,14 +31,13 @@ const Home = () => {
             <svg className="absolute pointer-events-none top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" viewBox="0 0 160 160" width="200" height="160" xmlns="http://www.w3.org/2000/svg">
               <g transform="matrix(0.966, -0.1, 0.55, 0.433, 80, 80)">
                 <path d="M 0,70 A 65,70 0 0,0 65,0 5,5 0 0,1 75,0 75,70 0 0,1 0,70Z" fill="#98c0ef" opacity="0.6">
-                  <animateTransform attributeName="transform" type="rotate" from="360 0 0" to="0 0 0" dur="7s" repeatCount="indefinite" />
+                  <animateTransform attributeName="transform" type="rotate" from="360 0 0" to="0 0 0" dur="4s" repeatCount="indefinite" />
                 </path>
               </g>
             </svg>
               View Resumé <span>{Icons.ArrowTopRight("size-4")}</span>
             </a>
           </div>
-        </div>
       </header>
       <SkillSummary />
       <ProjectsSummary />
